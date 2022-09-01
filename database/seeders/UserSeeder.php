368683c02c8d7de8415101970204223400cfb7d5
Ahmad Gozali <58220137+gozali97@users.noreply.github.com>
2022-09-01 09:02:20 +0700

    first

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,10 +2,9 @@
 
 namespace Database\Seeders;
 
-use DateTime;
+
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Str;
 
 class UserSeeder extends Seeder
 {
@@ -17,16 +16,16 @@ class UserSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            'name' => Str::random(10) . 'ahmad',
-            'email' => Str::random(10) . 'ahmad@gmail.com',
+            'name' => 'ahmad',
+            'email' => 'ahmad@gmail.com',
             'password' => bcrypt('admin'),
-            'hak_akses' => Str::random(10) . '0',
-            'province_id' => Str::random(50) . '20',
-            'city_id' => Str::random(50) . '20',
-            'jekel' => Str::random(50) . 'laki-laki',
-            'nama_instansi' => Str::random(50) . 'UTY',
-            'jabatan' => Str::random(50) . 'staf',
-            'no_tlp' => Str::random(50) . '0918431',
+            'hak_akses' => '0',
+            'province_id' => '20',
+            'city_id' => '20',
+            'jekel' => 'laki-laki',
+            'nama_instansi' => 'UTY',
+            'jabatan' => 'staf',
+            'no_tlp' => '0918431',
         ]);
     }
 }
